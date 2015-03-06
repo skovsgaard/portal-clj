1e99fcf5c492a500ef28234eac3d706a35725cd6
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2015-03-06 14:34:57 +0100

    Visual tweaks to post listings.

--- src/portal_clj/views/base.clj
+++ src/portal_clj/views/base.clj
@@ -3,18 +3,19 @@
   (:require [portal-clj.views.layout :as layout]))
 
 (defn post-item [post]
-  [:article
+  [:article.blog-post
    [:h2 (get post :title)]
    [:p (get post :body)]
-   [:p (get post :created_at)]])
+   [:p.timestamp (get post :created_at)]])
 
 (defn index [posts]
   [:section.wrap
    [:header.main-header
      [:span.title "SBC - Single Board Computers"]
      (layout/menu [["/" "Something"]
-                 ["//google.com" "something"]
-                 ["#" "something else"]])]
+                   ["//google.com" "something"]
+                   ["#" "something else"]])]
    [:section.blog
+    [:h1 "Latest news!"]
     (for [x posts] (post-item x))]
     layout/footer])
