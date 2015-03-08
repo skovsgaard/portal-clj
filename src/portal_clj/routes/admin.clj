6ba1a47b3c5e9179b672ab6bb39d66965310ebc6
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2015-03-08 18:30:45 +0100

    Added session state to /admin routes

--- src/portal_clj/routes/admin.clj
+++ src/portal_clj/routes/admin.clj
@@ -4,12 +4,18 @@
             [portal-clj.views.admin :as admin]
             [portal-clj.models.user :as user]))
 
-(defn home []
-  (layout/common (admin/index (user/all))))
+(defn with-session [template session]
+  {:body template
+   :session session})
 
-(defn do-post []
-  (layout/common (admin/post-post)))
+(defn home [session]
+  (-> (layout/common (admin/index (user/all)))
+      (with-session session)))
+
+(defn do-post [req]
+  (-> (layout/common (admin/post-post))
+      (with-session session)))
 
 (defroutes admin-routes
-  (GET "/admin/home" [] (home))
-  (POST "/admin/post" [] (do-post)))
+  (GET "/admin/home" {session :session} (home session))
+  (POST "/admin/post" req (do-post req)))
