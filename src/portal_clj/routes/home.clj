25bbe1c107afff36a8149b0b8cda89e813447537
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2015-03-08 17:57:43 +0100

    Added sesssion state for home routes

--- src/portal_clj/routes/home.clj
+++ src/portal_clj/routes/home.clj
@@ -1,5 +1,6 @@
 (ns portal-clj.routes.home
   (:require [compojure.core :refer :all]
+            [ring.middleware.session :as session]
             [portal-clj.routes.admin :as admin]
             [portal-clj.routes.user :as user]
             [portal-clj.views.layout :as layout]
@@ -7,27 +8,37 @@
             [portal-clj.models.post :as post]
             [portal-clj.models.tag :as tag]))
 
-(defn home []
-  (layout/common (base/index (post/all))))
+(defn with-session [template session]
+  {:body template
+   :session session})
 
-(defn about []
-  (layout/common (base/about)))
+(defn home [session]
+  (-> (layout/common (base/index (post/all)))
+      (with-session session)))
 
-(defn tags []
-  (layout/common (base/tags (tag/all))))
+(defn about [session]
+  (-> (layout/common (base/about))
+      (with-session session)))
 
-(defn login []
-  (layout/common (base/login)))
+(defn tags [session]
+  (-> (layout/common (base/tags (tag/all)))
+      (with-session session)))
 
-(defn do-login [user-credentials]
-  (let [params (get user-credentials :params)]
+(defn login [session]
+  (-> (layout/common (base/login))
+      (with-session session)))
+
+(defn do-login [req]
+  (let [params (get req :params)
+        session (get req :session)]
     (if (= (get params :email) "idi@admin.com")
-      (layout/common (admin/home))
+      (-> (layout/common (admin/home))
+          (with-session session))
       (layout/common (user/home)))))
 
 (defroutes home-routes
-  (GET "/" [] (home))
-  (GET "/page/about" [] (about))
-  (GET "/page/tags" [] (tags))
-  (GET "/page/login" [] (login))
-  (POST "/page/login" user-cred (do-login user-cred)))
+  (GET "/" {session :session} (home session))
+  (GET "/page/about" {session :session} (about session))
+  (GET "/page/tags" {session :session} (tags session))
+  (GET "/page/login" {session :session} (login session))
+  (POST "/page/login" req (do-login req)))
