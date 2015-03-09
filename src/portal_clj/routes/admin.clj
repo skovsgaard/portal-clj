2596c574c0624891873aac49655eb8fde438fbf5
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2015-03-09 03:14:33 +0100

    Added posting from admin.

--- src/portal_clj/routes/admin.clj
+++ src/portal_clj/routes/admin.clj
@@ -1,8 +1,14 @@
 (ns portal-clj.routes.admin
   (:require [compojure.core :refer :all]
+            [clojure.string :as string]
             [portal-clj.views.layout :as layout]
             [portal-clj.views.admin :as admin]
-            [portal-clj.models.user :as user]))
+            [portal-clj.models.user :as user]
+            [portal-clj.models.post :as post]))
+
+(defn sluggify [title]
+  (-> (string/lower-case title)
+      (string/replace #" " "-")))
 
 (defn with-session [template session]
   {:body template
@@ -13,6 +19,14 @@
         (with-session session)))
 
 (defn do-post [req]
+  (let [post (get req :params)]
+    (post/create! {:title (get post :title)
+                   :body (get post :post-content)
+                   :user_id (-> (get req :session) (get :user))
+                   :commentable 0
+                   :slug (sluggify (get post :title))
+                   :active 1
+                   :restricted 0}))
   (-> (layout/common (admin/post-post (get req :session)))
       (with-session (get req :session))))
 
