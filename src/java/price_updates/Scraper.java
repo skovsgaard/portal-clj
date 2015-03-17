4d13f19e094651797dd09c863c8cc06a6c7687d6
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2015-03-17 18:15:07 +0100

    Added Scraper class

--- src/java/price_updates/Scraper.java
+++ src/java/price_updates/Scraper.java
@@ -6,12 +6,36 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Scraper {
-  public static String get(String addr, String elem) {
+  private static String get(String addr, String elem) {
     try {
       Document doc = Jsoup.connect(addr).get();
       return doc.select(elem).text();
     } catch (Exception e) {
       return e.toString();
     }
+  }
+
+  private static double calcPrice(String priceStr, int quantity) {
+    return Double.parseDouble(priceStr) * (double)quantity;
+  }
+
+  public static String getRaspberryPi(int quantity) {
+    String addr = "http://www.amazon.com/s/&url=search-alias=aps&field-keywords=raspberry%20pi%202?tag=duckduckgo-20";
+    String priceStr = get(addr, "span.a-color-price").split(" ")[0];
+    priceStr = priceStr.substring(priceStr.indexOf("$")+1);
+    return Double.toString(calcPrice(priceStr, quantity));
+  }
+
+  public static String getParallella(int quantity) {
+    String addr = "http://www.digikey.com/product-search/en?keywords=1554-1001-ND&WT.z_slp_buy=Adapteva_Parallella";
+    String priceStr = get(addr, "table#pricing td[align=right]").split(" ")[0];
+    return Double.toString(calcPrice(priceStr, quantity));
+  }
+
+  public static String getBeagleBone(int quantity) {
+    String addr = "https://www.adafruit.com/products/1876";
+    String priceStr = get(addr, "#prod-price");
+    priceStr = priceStr.split(" ")[0].substring(priceStr.indexOf("$")+1);
+    return Double.toString(calcPrice(priceStr, quantity));
   }
 }
--- src/portal_clj/tester.clj
+++
@@ -1,5 +0,0 @@
-(ns portal-clj.tester
-  (:import [price_updates Scraper]))
-
-(defn -main [& args]
-  (println (Scraper/get "http://fupifarvandet.dk" "a")))
